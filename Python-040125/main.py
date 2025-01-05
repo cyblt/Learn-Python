31971238313c6ed8070a4b366f765aced244f5fd
cyblt <130627126+cyblt@users.noreply.github.com>
2025-01-05 18:20:49 +0700

    Update main.py

--- Python-040125/main.py
+++ Python-040125/main.py
@@ -1,8 +1,11 @@
 print("Hello World")
 
 #Python Indentation
-if 5 < 2 :
+if 5 > 2 :
     ("Angka 5 lebih besar dari 2")
+
 #Contoh salah
 # 5 < 2 :
 #print("Angka 5 lebih besar dari 2")
+
+#Kenapa salah? karena pada Python ada yang disebut Indentation yang mana mengharuskan ruang spasi setelah baris if minimal satu spasi, normalnya adalah empat
