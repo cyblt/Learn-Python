4a72a4a6721719d1ea974d89f4ce1a1aeb533e50
cyblt <130627126+cyblt@users.noreply.github.com>
2025-01-06 07:19:53 +0700

    Update main.py

--- Python-040125/main.py
+++ Python-040125/main.py
@@ -1,4 +1,5 @@
 print("Hello World")
+print("This is the Syntax Chapter")
 
 #Python Indentation
 if 5 > 2 :
@@ -9,3 +10,14 @@ if 5 > 2 :
 #print("Angka 5 lebih besar dari 2")
 
 #Kenapa salah? karena pada Python ada yang disebut Indentation yang mana mengharuskan ruang spasi setelah baris if minimal satu spasi, normalnya adalah empat
+
+#Python Variable
+#In Python, variables are created when you assign a value to it: 
+x = 5
+y = "Hello Python!!!"
+#Python has no command for declaring a variable.
+
+#Python Conmments
+#This is Comment
+#Python has commenting capability for the purpose of in-code documentation.
+#Comments start with a #, and Python will render the rest of the line as a comment: 
