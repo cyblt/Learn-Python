f4218aab5d5885430d542e4dd7c88b22bebf266b
cyblt <130627126+cyblt@users.noreply.github.com>
2025-01-07 13:29:55 +0700

    Create main.py

---
+++ Python-7125/main.py
@@ -0,0 +1,18 @@
+#Python Variable
+
+#Variable are containers for storing data values
+"""
+A variable is created the moment you first assign
+a value to it.
+"""
+#example
+x = 5
+y = 3
+print(x)
+print(y)
+
+#Variables do not need to be declared with any particular type. and can even change type after they have been set.
+#example
+x = 5 # x is of type int
+x = "five" # x is now of type str
+print(x)
