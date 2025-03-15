ca831d36c20628d1b09f5b6103e5d3d20d9b883c
cyblt <muhammadashfan32@gmail.com>
2025-03-16 06:06:08 +0700

    menambahkan direktori dan main.py

---
+++ Python-170225/main.py
@@ -0,0 +1,66 @@
+# Python Numbers
+print(10*'=', "Python Numbers", 10*'=')
+
+print("There are three numeric types in Python:")
+
+print("""
+1. int
+2. float
+3. complex
+""")
+
+print("Variables of numeric types are created when you assign a value to them:")
+
+print("""
+Example:
+x = 1    # int
+y = 2.8  # float
+z = 1j   # complex
+""")
+
+print("""
+To verify the type of any object in Python, use the type() function:
+
+Example:
+print(type(x))
+print(type(y))
+print(type(z))
+""")
+
+print('\n' + 10*'=', "int", 10*'=')
+
+print("Int, or integer, is a whole number, positive or negative, without decimals, of unlimited length.")
+
+print("""
+Example
+
+Integers:
+x = 1
+y = 35656222554887711
+z = -3255522
+
+print(type(x))
+print(type(y))
+print(type(z)) 
+""")
+
+print('\n' + 10*'=', "float", 10*'=')
+
+print('Float, or "floating point number" is a number, positive or negative, containing one or more decimals.')
+
+print("""
+Example
+
+Floats:
+x = 1.10
+y = 1.0
+z = -35.59
+
+print(type(x))
+print(type(y))
+print(type(z))
+""")
+
+print('Float can also be scientific numbers with an "e" to indicate the power of 10.')
+
+
